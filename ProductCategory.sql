93a0509c7b547876aedbe3784fd166eea212c064
Maxim Mustafin <mustafin.m95@gmail.com>
2024-09-11 09:32:29 +0300

    Product category sql

---
+++ ProductCategory.sql
@@ -0,0 +1,9 @@
+SELECT 
+    p.ProductName AS ProductName, 
+    c.CategoryName AS CategoryName
+FROM 
+    Products p
+LEFT JOIN 
+    ProductCategories pc ON p.ProductID = pc.ProductID
+LEFT JOIN 
+    Categories c ON pc.CategoryID = c.CategoryID
